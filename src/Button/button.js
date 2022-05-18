563cd637c4535c2551252335eb0ad78eda3aa0c1
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-05-18 17:04:29 -0200

    Deleted the ButtonColor function and implemented the button colors to be equal to the background colors

--- src/Button/button.js
+++ src/Button/button.js
@@ -1,15 +1,6 @@
 import React from "react";
 import styles from "./button.module.css";
 
-export default function Button({ buttonColor }) {
-  return (
-    <button
-      className={styles.buttonStyle}
-      style={{
-        color: buttonColor,
-      }}
-    >
-      Learn More
-    </button>
-  );
+export default function Button() {
+  return <button className={styles.buttonStyle}>Learn More</button>;
 }
--- src/Card/card.js
+++ src/Card/card.js
@@ -21,26 +21,8 @@ export default function Card({
         <img src={imgSource} alt="car image" />
         <h2 className={styles.headerStyle}>{title}</h2>
         <p className={styles.bodyStyle}>{content}</p>
-        <Button buttonColor={ButtonColor(backgroundColor)} />
+        <Button buttonColor={backgroundColor} />
       </div>
     </>
   );
-}
-
-function ButtonColor(backgroundColor) {
-  let buttonColor;
-
-  if (backgroundColor === "hsl(31, 77%, 52%)") {
-    buttonColor = "hsl(31, 77%, 52%)";
-  }
-
-  if (backgroundColor === "hsl(184, 100%, 22%)") {
-    buttonColor = "hsl(184, 100%, 22%)";
-  }
-
-  if (backgroundColor === "hsl(179, 100%, 13%)") {
-    buttonColor = "hsl(179, 100%, 13%)";
-  }
-
-  return buttonColor;
 }
